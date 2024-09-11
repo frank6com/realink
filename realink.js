97afc0501d1ddd7313a312bcca15aae089e01d23
Frank6 <liuchunbo@gmail.com>
2024-09-12 00:47:14 +0800

    修改代码内容

--- realink.js
+++ realink.js
@@ -1,100 +1,23 @@
 // ==UserScript==
-// @name         日期格式转换
-// @namespace    http://frank6.com/
-// @version      1.2
-// @description  将英文版的日期格式转换为2024年9月10日这种格式
+// @name         真实链接替换
+// @namespace    https://www.frank6.com/
+// @version      0.4
+// @description  将页面中所有链接文本为网址，href却不一致的a标签替换的小工具
 // @author       Frank6
-// @match        *://*/*
 // @grant        none
+// @include      http*
 // @license      MIT
+// @downloadURL https://update.greasyfork.org/scripts/394114/%E7%9C%9F%E5%AE%9E%E9%93%BE%E6%8E%A5%E6%9B%BF%E6%8D%A2.user.js
+// @updateURL https://update.greasyfork.org/scripts/394114/%E7%9C%9F%E5%AE%9E%E9%93%BE%E6%8E%A5%E6%9B%BF%E6%8D%A2.meta.js
 // ==/UserScript==
-
-(function () {
+(function() {
     'use strict';
+    let urlArr = document.getElementsByTagName('a');
 
-    // 匹配常见的英文日期格式：09/10/2024, September 10, 2024, Sep 10, 2024, 17 August 2024
-    const dateRegex = /(\b\d{1,2}\/\d{1,2}\/\d{4}\b|\b\w+\s\d{1,2},\s\d{4}\b|\b\w{3}\s\d{1,2},\s\d{4}\b|\b\d{1,2}\s\w+\s\d{4}\b)/g;
-
-
-    // 获取页面中的所有文本节点
-    function walk(node) {
-        let child, next;
-        switch (node.nodeType) {
-            case 1: // Element
-            case 9: // Document
-            case 11: // Document fragment
-                child = node.firstChild;
-                while (child) {
-                    next = child.nextSibling;
-                    walk(child);
-                    child = next;
-                }
-                break;
-            case 3: // Text node
-                handleText(node);
-                break;
+    for(let item of urlArr){
+        let regex = /^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/i;
+        if(regex.test(item.text)){
+            item.href = item.text;
         }
     }
-
-    // 处理文本节点
-    function handleText(textNode) {
-        let text = textNode.nodeValue;
-        textNode.nodeValue = text.replace(dateRegex, function (match) {
-            return convertDate(match);
-        });
-    }
-
-    // 将英文日期格式转换为中文日期格式
-    function convertDate(dateStr) {
-        const fullMonths = {
-            January: 1, February: 2, March: 3, April: 4, May: 5, June: 6,
-            July: 7, August: 8, September: 9, October: 10, November: 11, December: 12
-        };
-
-        const shortMonths = {
-            Jan: 1, Feb: 2, Mar: 3, Apr: 4, May: 5, Jun: 6,
-            Jul: 7, Aug: 8, Sep: 9, Oct: 10, Nov: 11, Dec: 12
-        };
-
-        // 处理 "September 10, 2024" 格式
-        let longDateMatch = /(\w+)\s(\d{1,2}),\s(\d{4})/.exec(dateStr);
-        if (longDateMatch && fullMonths[longDateMatch[1]]) {
-            let month = fullMonths[longDateMatch[1]];
-            let day = longDateMatch[2];
-            let year = longDateMatch[3];
-            return `${year}年${month}月${day}日`;
-        }
-
-        // 处理 "Sep 10, 2024" 格式
-        let shortDateMatch = /(\w{3})\s(\d{1,2}),\s(\d{4})/.exec(dateStr);
-        if (shortDateMatch && shortMonths[shortDateMatch[1]]) {
-            let month = shortMonths[shortDateMatch[1]];
-            let day = shortDateMatch[2];
-            let year = shortDateMatch[3];
-            return `${year}年${month}月${day}日`;
-        }
-
-        // 处理 "09/10/2024" 格式
-        let slashDateMatch = /(\d{1,2})\/(\d{1,2})\/(\d{4})/.exec(dateStr);
-        if (slashDateMatch) {
-            let month = slashDateMatch[1];
-            let day = slashDateMatch[2];
-            let year = slashDateMatch[3];
-            return `${year}年${month}月${day}日`;
-        }
-
-        // 处理 "17 August 2024" 格式
-        let reverseDateMatch = /(\d{1,2})\s(\w+)\s(\d{4})/.exec(dateStr);
-        if (reverseDateMatch && fullMonths[reverseDateMatch[2]]) {
-            let day = reverseDateMatch[1];
-            let month = fullMonths[reverseDateMatch[2]];
-            let year = reverseDateMatch[3];
-            return `${year}年${month}月${day}日`;
-        }
-
-        return dateStr; // 如果无法匹配，则返回原始文本
-    }
-
-    // 执行
-    walk(document.body);
 })();
